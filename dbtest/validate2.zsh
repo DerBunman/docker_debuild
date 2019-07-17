cdac71162ec4e20da72af3ebd4b41163ec38f001
DerBunman <lw200552@googlemail.com>
2019-07-17 20:23:22 +0200

    test

--- dbtest/validate2.zsh
+++ dbtest/validate2.zsh
@@ -4,12 +4,12 @@
 	#xdotool windowunmap $(xdotool search --classname "xterm")
 	xdotool windowunmap $(xdotool search --classname "xterm")
 	i3 &
-	sleep 20
-	i3-msg restart
-	urxvt -e zsh -c "neofetch; sleep 120" &
 	sleep 5
 	i3-msg restart
-	sleep 10
+	urxvt -e zsh -c "neofetch; sleep 120" &
+	#sleep 5
+	i3-msg restart
+	sleep 5
 	img=neofetch1.png;
 	scrot $img;
 	curl -s -X POST -F "image=@$img" https://doublefun.net/media/x.php
